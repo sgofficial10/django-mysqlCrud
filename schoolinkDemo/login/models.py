7045f3d7774229739f863931216f98509d44ed8e
usera <sujay.ghosh@lykapp.com>
2019-10-22 00:28:35 +0530

    make hashed password

--- schoolinkDemo/login/models.py
+++ schoolinkDemo/login/models.py
@@ -3,16 +3,16 @@ from datetime import datetime
 from django.utils import timezone
 # Create your models here.
 class User(models.Model):
-    gcm_id = models.IntegerField()
+    gcm_id = models.IntegerField(default=0)
     fname = models.CharField(max_length = 255)
     deviceType = models.CharField(max_length=150)
     active = models.IntegerField(default=0)
     userType = models.CharField(max_length=10)
     email = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
+    password = models.CharField(max_length=500)
     rememberToken = models.TextField()
     createdAt = models.DateTimeField(default=timezone.now)
-    isDelete = models.CharField(max_length=10)
+    isDelete = models.CharField(max_length=10, default='0')
 
 
 
--- schoolinkDemo/login/views.py
+++ schoolinkDemo/login/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password, check_password
 
 # Create your views here.
 
 
 def index(request):
-    if(request.method == 'POST' ):
-        return render(request , 'test.html')
-    else:
-        return render(request, 'login.html')
+    try:
+        if(request.method == 'POST' ):
+            if(request.POST.get('csrfmiddlewaretoken') is not None):
+                if(request.POST.get('Login') == 'Login'):
+                    email = request.POST.get('email').strip()
+                    password = request.POST.get('password').strip()
+                    if(email is None):
+                        return render(request, 'login.html', {'error' : 'Email is required.'})
+                    if(password is None):
+                        return render(request, 'login.html', {'error' : 'Password is required.'})
+                    hashedPassword = make_password(request.POST.get('password'))
+                    return render(request, 'test.html', {'hashedPassword': hashedPassword})
+                else:
+                    return render(request, 'login.html')
+            else:
+                return render(request, 'login.html')
+        else:
+            return render(request, 'login.html')
+    except:
+        return render(request, 'login.html', {'error': 'something went wrong.'})
